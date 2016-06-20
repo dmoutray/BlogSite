849e5634f1ddd4d0204d253e22a7bbbf5ce18930
dmoutray <dmoutray@live.co.uk>
2016-06-20 23:54:05 +0100

    Add files via upload

---
+++ start.php
@@ -0,0 +1,35 @@
+<?php
+
+ini_set('display_errors', 'On');
+
+define('APP_ROOT', __DIR__);
+define('BASE_URL', 'http://127.0.0.1/Tuesday/');
+
+$db = new PDO ('mysql:host=127.0.0.1; dbname=Pages', 'root', 'HSV5aqq7wdpb4ZnA');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
